65d0049d43015d15dfef71ce43ff938391108e71
G-Ray-Cell <adegokesamuel540@gmail.com>
2021-01-31 20:25:35 +0100

    test

---
+++ age.js
@@ -0,0 +1,6 @@
+console.log('Stating node.js');
+
+module.exports.addNote = function () {
+    console.log  
+
+}
---
+++ notes.js
@@ -0,0 +1,7 @@
+let samsungGalaxy = 45;
+let gioneeX = 34;
+let iPhone = 123;
+
+iPhone += 34;
+gioneeX += 123;
+samsungGalaxy += 34;
